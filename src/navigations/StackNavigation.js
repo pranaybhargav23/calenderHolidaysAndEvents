86f89c5c012e0ce2c4800068d0a8de0ea8242196
bhargav23032006 <bhargav23032006@gmail.com>
2025-09-15 02:11:00 +0530

    finshed

--- src/navigations/StackNavigation.js
+++ src/navigations/StackNavigation.js
@@ -2,7 +2,6 @@ import React from 'react'
 import SplashScreen from '../screens/SplashScreen';
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
-import EventList from '../screens/EventList';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
@@ -14,7 +13,6 @@ const StackNavigation = () => {
         <Stack.Screen name="SplashScreen" component={SplashScreen}/>
         <Stack.Screen name="LoginScreen" component={LoginScreen}/>
         <Stack.Screen name="HomeScreen" component={HomeScreen}/>
-        <Stack.Screen name="EventList" component={EventList}/>
     </Stack.Navigator>
   )
 }
